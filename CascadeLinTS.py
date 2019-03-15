a7d74a0a524c7973f55ffc02c288f57f8546333f
Shuai Li <shuaili@Shuais-MacBook-Pro.local>
2019-03-15 10:50:25 +0800

    add CascadeLinTS

--- CascadeLinTS.py
+++ CascadeLinTS.py
@@ -2,19 +2,17 @@ import numpy as np
 import sys
 from utlis import is_power2
 
-class CascadeLinUCB(object):
+class CascadeLinTS:
     # Current version is for fixed item set
     def __init__(self, K, env, T):
-        super(CascadeLinUCB, self).__init__()
+        super(CascadeLinTS, self).__init__()
         self.K = K
         self.env = env
         self.T = T
 
         self.items = self.env.items
         self.d = len(self.items[0])
-        self.beta = np.sqrt(self.d * np.log((1 + self.T/self.d) + 2*np.log(4*self.T))) + 1
-
-        # L = len(self.items)
+        
         self.S = np.zeros((self.d, self.d))
         self.b = np.zeros(self.d)
         self.Sinv = np.zeros((self.d, self.d))
@@ -26,17 +24,10 @@ class CascadeLinUCB(object):
         for t in range(self.T):
             if True:#t % 5000 == 0 or is_power2(t):
                 self.Sinv = np.linalg.pinv(self.S)
-                self.theta = np.dot(self.Sinv, self.b)
+                theta = np.dot(self.Sinv, self.b)
+                self.theta = np.random.multivariate_normal(theta, self.Sinv)
 
-            At = np.argsort(np.dot(self.items, self.theta) + self.beta * (np.matmul(self.items, self.Sinv) * self.items).sum(axis = 1))[:: -1][: self.K]
-            
-            # if np.linalg.cond(self.S) < 1 / sys.float_info.epsilon:
-            # 	Sinv = np.linalg.inv(self.S)
-            # 	theta = np.dot(Sinv, self.b)
-
-            # 	At = np.argsort(np.dot(self.items, theta) + self.beta * (np.matmul(self.items, Sinv) * self.items).sum(axis = 1))[:: -1][: self.K]
-            # else:
-            # 	At = np.random.permutation(len(self.items))[: self.K]
+            At = np.argsort(np.dot(self.items, self.theta))[:: -1][: self.K]
 
             x, r = self.env.feedback(At)
             self.rewards[t] = r
